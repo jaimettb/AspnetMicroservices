1128d8e0a5d0f24e7b14f43b8a81437a6ee8fa53
Jaime <jaimettb@gmail.com>
2022-10-27 12:32:06 -0300

    Implemented DiscountService. Missing AutoMapper.

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,34 @@
+﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService: DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _repository;
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository repository, ILogger<DiscountService> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository)) ;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public override async Task<CuponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var cupom = await _repository.GetDiscount(request.ProductName);
+            
+            if(cupom == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={request.ProductName} not found"));
+            }
+
+            var cupomModel = _mapper.Map<CuponModel>(cupom);
+            return cupomModel;
+        }
+    }
+}
