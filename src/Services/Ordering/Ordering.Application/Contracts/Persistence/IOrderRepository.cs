106be34da34db6ddbc4a3aa947274471e1d69595
Jaime <jaimettb@gmail.com>
2022-11-04 12:44:52 -0300

    CQRS with MediatR in Ordering

--- src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Contracts.Persistence
 {
-    public interface IOrderingRepository : IAsyncRepository<Order>
+    public interface IOrderRepository : IAsyncRepository<Order>
     {
         Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
     }
