84a28e3a845eafa5ce49b807c1a23d84b22949d2
Jaime <jaimettb@gmail.com>
2022-11-04 15:03:15 -0300

    CheckoutOrderCommand implemented

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infraestructure/IEMailService.cs
@@ -0,0 +1,10 @@
+﻿using Ordering.Application.Models;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Infraestructure
+{
+    public interface IEMailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
