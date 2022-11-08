daaa8d17ee81e887186b5f209ee3df4aecd865ea
Jaime <jaimettb@gmail.com>
2022-11-08 14:52:47 -0300

    Started event bus implementation

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreateDate = DateTime.Now;
+        }
+        public IntegrationBaseEvent(Guid id, DateTime createDate)
+        {
+            Id = id;
+            CreateDate = createDate;
+        }
+    }
+}
