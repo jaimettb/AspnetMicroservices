ff1fac886fd6e51a86e0a14eb38ed3e85e151b59
Jaime <jaimettb@gmail.com>
2022-11-10 16:35:00 -0300

    Started OcelotApi Gatway project

---
+++ src/ApiGateways/OceloApiGw/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace OcelotApiGw
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot();
+                //.AddCacheManager(settings => settings.WithDictionaryHandle());
+        }
+
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+
+            await app.UseOcelot();
+        }
+    }
+}
