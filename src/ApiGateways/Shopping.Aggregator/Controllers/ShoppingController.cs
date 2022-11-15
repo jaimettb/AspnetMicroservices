3a9b008702ac2dc44a5fe89660756608492ad12e
Jaime <jaimettb@gmail.com>
2022-11-15 08:42:14 -0300

    Finalized Shopping Aggregator

--- src/ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
+++ src/ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shopping.Aggregator.Models;
 using Shopping.Aggregator.Services;
+using System.Net;
 
 namespace Shopping.Aggregator.Controllers
 {
@@ -8,20 +9,50 @@ namespace Shopping.Aggregator.Controllers
     [Route("api/v1/[controller]")]
     public class ShoppingController: ControllerBase
     {
-        private readonly ICatalogService catalogService;
-        private readonly IBasketService basketService;
-        private readonly IOrderService orderService;
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderService;
 
         public ShoppingController(ICatalogService catalogService, IBasketService basketService, IOrderService orderService)
         {
-            this.catalogService = catalogService;
-            this.basketService = basketService;
-            this.orderService = orderService;
+            this._catalogService = catalogService;
+            this._basketService = basketService;
+            this._orderService = orderService;
         }
 
+        [HttpGet("{userName}", Name = "GetShopping")]
+        [ProducesResponseType(typeof(ShoppingModel), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingModel>> GetShopping(string userName)
         {
+            // Get basket with username
+            // interate basket items and consume products with basket item productId member
+            // map product related members into basketitem dto with extended columns
+            // consume ordering microservices in order to retrieve order list
+            // return root ShoppingModel dto class wich including all responses
 
+            var basket = await _basketService.GetBasket(userName);
+            foreach(var item in basket.Items)
+            {
+                var product = await _catalogService.GetCatalog(item.ProductId);
+
+                // set aditional product fields onto basket item
+                item.ProductName = product.Name;
+                item.Category = product.Category;
+                item.Summary = product.Summary;
+                item.Description = product.Description;
+                item.ImageFile = product.ImageFile;
+            }
+
+            var orders = await _orderService.GetOrdersByUserName(userName);
+
+            var shoppingModel = new ShoppingModel()
+            {
+                UserName = userName,
+                BasketWithProducts = basket,
+                Orders = orders
+            };
+
+            return Ok(shoppingModel);
         }
     }
 }
